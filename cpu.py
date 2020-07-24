15cfc3155785feeee06f879b2e4a990785899d40
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-24 10:02:36 -0700

    cleaned and commented

--- cpu.py
+++ cpu.py
@@ -39,7 +39,6 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
         if program == None:
             program = [
                 # From print8.ls8
@@ -72,7 +71,7 @@ class CPU:
 
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU operations. CMP does a comparison"""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
@@ -83,21 +82,12 @@ class CPU:
         elif op == "DIV":
             self.reg[reg_a] /= self.reg[reg_b]
         elif op == "CMP":
-            #print(reg_a, reg_b)
             if reg_a == reg_b:
-                #print(self.fl, "equal before")
                 self.fl = EM
-                #print(self.fl,'after', bin(self.fl))
             elif reg_a > reg_b:
-                #print(self.fl, "greater before")
                 self.fl = GTM
-                #print(self.fl,'after', bin(self.fl))
-
             elif reg_a < reg_b:
-                #print(self.fl, "less before")
                 self.fl = LTM
-                #print(self.fl,'after', bin(self.fl))
-
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -109,8 +99,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -124,7 +112,6 @@ class CPU:
     def run(self):
         """Run the CPU."""
         run = True
-        #self.pc = 0 #program counter
 
         while run == True:
             instruction = self.ram_read(self.pc)
@@ -134,40 +121,31 @@ class CPU:
                 run = False
                 print('halt!')
             elif instruction == LDI:
-                #print(operand_a, operand_b)
-                #print('Store:', operand_b, 'in', operand_a)
                 self.reg[operand_a] = operand_b
                 self.pc += 3
             elif instruction == SUB:
                 print('Subtract:', self.reg[operand_a], 'and', self.reg[operand_b])
                 self.alu('SUB', operand_a, operand_b)
-                #print('Result:', self.reg[operand_a])
                 self.pc += 3
             elif instruction == ADD:
                 print('Add:', self.reg[operand_a], 'and', self.reg[operand_b])
                 self.alu('ADD', operand_a, operand_b)
-                #print('Result:', self.reg[operand_a])
                 self.pc += 3
             elif instruction == MUL:
                 print('Multiply:',self.reg[operand_a], "and", self.reg[operand_b])
                 self.alu('MUL', operand_a, operand_b)
-                #print('Result:', self.reg[operand_a])
                 self.pc += 3
             elif instruction == DIV:
                 print('Divide:', self.reg[operand_a], 'by', self.reg[operand_b])
                 self.alu('DIV', operand_a, operand_b)
-                #print('Result:', self.reg[operand_a])
                 self.pc += 3
             elif instruction == PRN:
-                #print("\nPrint\n")
                 print(self.reg[operand_a])
                 self.pc += 2
             elif instruction == PUSH:
                 if self.reg[SP]!=0x00:
-
                     self.reg[SP] -= 1
                     self.ram_write(self.reg[SP], self.reg[operand_a])
-                    #print('Push:', self.reg[operand_a], 'to', self.reg[SP])
                 else:
                     print("stack full!")
                 self.pc += 2
@@ -175,42 +153,31 @@ class CPU:
                 if self.reg[SP] == 0xF4:
                     print("can't pop without a stack!")
                 else:
-                    #print('Pop:', self.ram_read(self.reg[SP]), 'from', self.reg[SP])
                     self.reg[operand_a] = self.ram_read(self.reg[SP])
                     self.reg[SP] +=1
                 self.pc +=2
             elif instruction == CALL:
-                #print('CALL --->', self.reg[operand_a])
                 self.ram_write(self.reg[SP], self.pc + 2)
                 self.reg[SP] -= 1
                 self.pc = self.ram_read(operand_a + 1)
 
             elif instruction == RET:
-                #print('RET --->', self.ram_read(self.reg[SP] + 1))
-
                 self.pc = self.ram_read(self.reg[SP] + 1)
                 self.reg[SP] += 1
             elif instruction == CMP:
-                #print("CMP!", self.reg[operand_a], self.reg[operand_b])
                 self.alu('CMP', self.reg[operand_a], self.reg[operand_b])
                 self.pc += 3
             elif instruction == JMP:
-                #print('JMP!')
                 self.pc = self.reg[operand_a]
             elif instruction == JEQ:
-                #print("JEQ")
-                #print('fl:', self.fl, 'EM:', EM)
                 if self.fl == EM:
-                    #print(operand_a)
                     self.pc=self.reg[operand_a]
                 else:
                     self.pc += 2
             elif instruction == JNE:
-                #print("JNE")
                 if 0b1 & self.fl == 0b0:
                     self.pc =self.reg[operand_a]
                 else:
-                    #print(bin(self.fl), "equal")
                     self.pc += 2
 
             else:
@@ -232,4 +199,3 @@ if __name__ == "__main__":
     test = CPU()
     test.load(prog)
     test.run()
-    #print([x for x in prog])
